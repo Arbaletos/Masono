f97191120f4a5b6677c6ca81ce7d52621745481b
Arbaletos <artemioarbaletos@gmail.com>
2020-07-20 17:10:26 +0300

    remove data init from schema.sql

--- schema.sql
+++ schema.sql
@@ -11,11 +11,3 @@ create table respondoj (
   dem_id integer not null,
   respondo text not null
 );
-
-insert into demandoj(konteksto, demando)
-  values ('La Paŭlo estis vere sentema persono.',
-          'Ĉu korektaj elementoj por vorto "sentema" estas "sen_tem_a"');
-
-insert into demandoj(konteksto, demando)
-  values ('La diskuto pri projektoj rapide fariĝis sentema babilado',
-  'Ĉu korektaj elementoj por vorto "sentema" estas "sen_tem_a"?');
