31658fb2faa05fdc31e3d331bfd8760fa028a5df
Arbaletos <artemioarbaletos@gmail.com>
2020-07-20 17:28:02 +0300

    python script to export datumo

---
+++ eksportu.py
@@ -0,0 +1,14 @@
+import os
+import sqlite3
+
+def eksportu(datumo, datejo):
+  db = sqlite3.connect(datejo)
+  for line in open(datumo,'r',encoding='utf-8'):
+      kunteksto, demando = line.strip().split('\t')
+      db.cursor().execute('insert into demandoj(konteksto, demando) values(?, ?)', 
+                          [kunteksto, demando])
+  db.commit()
+
+
+if __name__=='__main__':
+    eksportu('en_datumo.csv', 'masono.db')
