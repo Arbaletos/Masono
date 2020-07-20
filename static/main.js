370c9e74efdcecbed679904ce618923368a1312e
Arbaletos <artemioarbaletos@gmail.com>
2020-07-20 17:09:51 +0300

    remove redundant alerts

--- static/main.js
+++ static/main.js
@@ -11,13 +11,11 @@
             $log.log(error);
           }
       $scope.ago= function(ago, dem_id) {
-        alert('putin')
         $http.post('/send',{'ago':ago,'dem_id':dem_id}).
           success(suc).
           error(err);
       };
       $scope.sekv= function() {
-        alert('shumok')
         $http.get('/sekv').
           success(suc).
           error(err);
